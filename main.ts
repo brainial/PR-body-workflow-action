12da7a4e085fcd4ea1e980d13472250a4f3dbe9a
Merlijn <merlijn@brainial.com>
2022-11-18 17:08:40 +0100

    just run

--- main.ts
+++ main.ts
@@ -1,20 +1,16 @@
-const github = require('@actions/github');
-const core = require('@actions/core');
+const github = require("@actions/github");
+const core = require("@actions/core");
 
-export const main = () => {
-  try {
-    const pattern = core.getInput('pattern');
-    const source = github.context.payload.pull_request?.body || '';
-    core.setOutput('pr_body', source);
+try {
+  const pattern = core.getInput("pattern");
+  const source = github.context.payload.pull_request?.body || "";
+  core.setOutput("pr_body", source);
 
-    const results = source.match(pattern) || [];
-    const match = results[1];
-    if (match) {
-      core.setOutput('value', match);
-    }
-  } catch (error) {
-    core.setFailed(error.message);
+  const results = source.match(pattern) || [];
+  const match = results[1];
+  if (match) {
+    core.setOutput("value", match);
   }
-};
-
-main();
+} catch (error) {
+  core.setFailed(error.message);
+}
